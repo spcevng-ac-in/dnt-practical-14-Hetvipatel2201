b0ca4be989cc3a748de832a16ea8d7369c4f2cac
Hetvipatel2201 <hetvipatel2271@gmail.com>
2022-04-27 21:55:48 +0530

    submit

---
+++ WebApplication13/Home.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="practical13.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+        <div>
+        </div>
+        <center><h1>
+&nbsp;Welcome to  <b style="color:red;"><asp:Label ID="welcomeLabel" runat="server"></asp:Label></b></h1>
+            <asp:Button ID="LogoutButton" runat="server" OnClick="LogoutButton_Click" Text="Logout" />
+    <form id="form1" runat="server">
+    
+    </center>
+        
+    </form>
+</body>
+</html>
---
+++ WebApplication13/Home.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace practical13
+{
+    public partial class home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["uname"] == null)
+            {
+                Response.Redirect("login.aspx");
+
+            }
+            welcomeLabel.Text = Session["uname"].ToString();
+        }
+
+        protected void LogoutButton_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("login.aspx");
+        }
+    }
+}
